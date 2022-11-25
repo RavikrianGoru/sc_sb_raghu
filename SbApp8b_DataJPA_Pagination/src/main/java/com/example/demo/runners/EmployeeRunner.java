7c5e2bf48a810980c49fc74a7aa18d1b0836f857
RavikiranGoru <ravikirangoru@gmail.com>
2022-11-25 15:09:14 +0530

    SbApp8b_DataJPA_Pagination updated

--- SbApp8b_DataJPA_Pagination/src/main/java/com/example/demo/runners/EmployeeRunner.java
+++ SbApp8b_DataJPA_Pagination/src/main/java/com/example/demo/runners/EmployeeRunner.java
@@ -8,6 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Component;
 
 import com.example.demo.entity.Employee;
@@ -24,59 +26,58 @@ public class EmployeeRunner implements CommandLineRunner {
 
 		// Save bulk Records
 		saveBuilkRecords();
-		
-		//List<Employee> allEmps=repo.findAll();
-		//System.out.println(allEmps);
 
-		long totalRows=repo.count();
-		System.out.println("Total Rows:"+totalRows);
-		
-		//Total rows 16, try with page 0,3,4
-		Pageable pageable=PageRequest.of(0, 5);
-		Page<Employee> page0=repo.findAll(pageable);
-		
-		//metadata
-		System.out.println(page0);
-		System.out.println("Page number:"+page0.getNumber());
-		System.out.println("Page size:"+page0.getSize());
-		System.out.println("No of elements in page:"+page0.getNumberOfElements());
-		System.out.println("Total no of elements(all rows):"+page0.getTotalElements());
-		System.out.println("Total no of pages:"+page0.getTotalPages());
-		System.out.println("Has Content:"+page0.hasContent());
-		System.out.println("Has Next:"+page0.hasNext());
-		System.out.println("Has Preavious:"+page0.hasPrevious());
-		System.out.println("Is Empty:"+page0.isEmpty());
-		System.out.println("Is First:"+page0.isFirst());
-		System.out.println("Is last:"+page0.isLast());
-		
-		//data
-		List<Employee>pageList=page0.getContent();
-		pageList.forEach(System.out::println);
-		
-		
-		
+		// List<Employee> allEmps=repo.findAll();
+		// System.out.println(allEmps);
+
+		long totalRows = repo.count();
+		System.out.println("Total Rows:" + totalRows);
+
+		// Total rows 16, try with page 0,3,4
+		// Pageable pageable=PageRequest.of(0, 5, Sort.by(Direction.ASC));
+		// Pageable pageable=PageRequest.of(0, 5, Sort.by(Direction.DESC));
+		// Pageable pageable=PageRequest.of(0, 5, Sort.unsorted());
+		//Pageable pageable = PageRequest.of(0, 5);
+
+		int recordSize = 5;
+		long noOfPages = totalRows / recordSize;
+		int eachPage = 0;
+		while (eachPage <= noOfPages) 
+		{
+			Pageable pageable = PageRequest.of(eachPage, recordSize);
+			Page<Employee> page0 = repo.findAll(pageable);
+
+			// metadata
+			System.out.println("Page details:" + page0);
+			System.out.println("Page number:" + page0.getNumber());
+			System.out.println("Page size:" + page0.getSize());
+			System.out.println("No of elements in page:" + page0.getNumberOfElements());
+			System.out.println("Total no of elements(all rows):" + page0.getTotalElements());
+			System.out.println("Total no of pages:" + page0.getTotalPages());
+			System.out.println("Has Content:" + page0.hasContent());
+			System.out.println("Has Next:" + page0.hasNext());
+			System.out.println("Has Preavious:" + page0.hasPrevious());
+			System.out.println("Is Empty:" + page0.isEmpty());
+			System.out.println("Is First:" + page0.isFirst());
+			System.out.println("Is last:" + page0.isLast());
+
+			// data
+			List<Employee> pageList = page0.getContent();
+			pageList.forEach(System.out::println);
+			
+			eachPage++;
+		}
+
 	}
 
-	private void saveBuilkRecords()
-	{
-		
-		List<Employee> list=Arrays.asList(new Employee(100,"Aaa",100.00),
-				new Employee(101,"Baa",130.00),
-				new Employee(102,"Caa",260.00),
-				new Employee(103,"Daa",700.00),
-				new Employee(104,"Eaa",140.00),
-				new Employee(105,"Faa",230.00),
-				new Employee(106,"Gaa",340.00),
-				new Employee(107,"Haa",120.00),
-				new Employee(108,"Iaa",540.00),
-				new Employee(109,"Jaa",40.00),
-				new Employee(110,"kaa",600.00),
-				new Employee(111,"Laa",300.00),
-				new Employee(112,"Maa",700.00),
-				new Employee(113,"Naa",140.00),
-				new Employee(114,"Oaa",105.00),
-				new Employee(115,"Paa",103.00)
-				);
+	private void saveBuilkRecords() {
+
+		List<Employee> list = Arrays.asList(new Employee(100, "Aaa", 100.00), new Employee(101, "Baa", 130.00),
+				new Employee(102, "Caa", 260.00), new Employee(103, "Daa", 700.00), new Employee(104, "Eaa", 140.00),
+				new Employee(105, "Faa", 230.00), new Employee(106, "Gaa", 340.00), new Employee(107, "Haa", 120.00),
+				new Employee(108, "Iaa", 540.00), new Employee(109, "Jaa", 40.00), new Employee(110, "kaa", 600.00),
+				new Employee(111, "Laa", 300.00), new Employee(112, "Maa", 700.00), new Employee(113, "Naa", 140.00),
+				new Employee(114, "Oaa", 105.00), new Employee(115, "Paa", 103.00));
 		repo.saveAll(list);
 	}
 
